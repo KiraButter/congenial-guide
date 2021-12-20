fa1d4c892d27b7037d9dba6664707311279453b7
Kira <oliviyazilber@mail.ru>
2021-12-20 18:28:56 +0300

    добавлена главная страница index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Some Page</title>
+</head>
+<body>
+    <main>
+        <h1>Начало работы</h1>
+    </main>
+</body>
+</html>
